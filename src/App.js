8c541e3a24a38545c0a6d2cb72b4b9b02416888c
M4dave <davidjohncruz@gmail.com>
2024-07-19 10:57:20 -0400

    added bubble sort need to fix CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LandingPage from './component/landingpage/LandingPage';
 import MiddleTitle from './component/middletitle/MiddleTitle';
+import BubbleSort from './component/bubblesort/BubbleSort';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
       <MiddleTitle /> 
+      <BubbleSort />
     </div>
   );
 }
--- src/component/bubblesort/BubbleSort.jsx
+++ src/component/bubblesort/BubbleSort.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import "./bubblesort.css"; // Import CSS file
 
 const BubbleSort = () => {
+  const BubbleVideo = process.env.PUBLIC_URL + "/bubblesort.mp4";
+
   return (
-    <div>
-      <div>
-        <h1>BubbleSort</h1>
-        <img 
+    <div className="bubble-container">
+      <h1 className="bubble-title">Bubble Sort</h1>
+      <div className="bubble-video-container">
+        <video className="bubble-vid" src={BubbleVideo} autoPlay loop muted />
       </div>
+      <p className="bubble-description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit.
+        </p>
     </div>
   );
 };
