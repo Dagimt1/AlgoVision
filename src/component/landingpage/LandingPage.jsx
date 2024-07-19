014ccb966eddf685b473b79a3024a519c77a0bc0
M4dave <davidjohncruz@gmail.com>
2024-07-19 07:50:52 -0400

    fixed landing page

--- src/component/landingpage/LandingPage.jsx
+++ src/component/landingpage/LandingPage.jsx
@@ -18,13 +18,16 @@ const LandingPage = () => {
           </h1>
           <p className="main-subtitle">
             Build computer science skills in minutes a day with bite-sized,
-            interactive problem solving.
           </p>
+          <p className="main-subtitle2"> interactive lessons you'll love.</p>
           <button className="main-get-started-btn">Get started</button>
         </div>
         <div className="gif-container">
           <video
-            src={process.env.PUBLIC_URL + '/0yEX6dfG1L8t8Nv9a2UX9k1DfI9lTOMX_9c5a0990aaa1b93e60a271db06dfbac9f2ad25e10bef32cd5cd54e19d46eabf5.mp4'}
+            src={
+              process.env.PUBLIC_URL +
+              "/0yEX6dfG1L8t8Nv9a2UX9k1DfI9lTOMX_9c5a0990aaa1b93e60a271db06dfbac9f2ad25e10bef32cd5cd54e19d46eabf5.mp4"
+            }
             autoPlay
             loop
             muted
