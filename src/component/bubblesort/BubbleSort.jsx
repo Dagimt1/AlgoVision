c4a902deb454647c1881993584cfb7b07b400064
M4dave <davidjohncruz@gmail.com>
2024-07-19 09:30:37 -0400

    tweaked landing video page

--- src/component/bubblesort/BubbleSort.jsx
+++ src/component/bubblesort/BubbleSort.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const BubbleSort = () => {
   return (
-    <div>BubbleSort</div>
-  )
-}
+    <div>
+      <div>
+        <h1>BubbleSort</h1>
+        <img 
+      </div>
+    </div>
+  );
+};
 
-export default BubbleSort
+export default BubbleSort;
