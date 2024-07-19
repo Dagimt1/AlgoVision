04f908caee91231bf29c2779f7d975552cbe470f
M4dave <davidjohncruz@gmail.com>
2024-07-19 17:31:10 -0400

    minor tweak bubblesort.css

--- src/component/insertsort/InsertSort.jsx
+++ src/component/insertsort/InsertSort.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import './insert.css'
+import React from "react";
+import "./insert.css";
 
 const InsertSort = () => {
   return (
-    <div>InsertSort</div>
-  )
-}
+    <div>
+      <h1>Welcome to Insert Sort</h1>
+    </div>
+  );
+};
 
-export default InsertSort
+export default InsertSort;
