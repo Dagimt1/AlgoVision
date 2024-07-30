3c1d0900afdcd172e95ee52e11917074dffdc905
Ashleigh McMahon <ashleighmc@me.com>
2024-07-30 04:26:01 +0200

    Adding Dijkstra Files

---
+++ src/component/PathfinderContainer/PathfinderContainer.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import './PathfinderContainer.css'; // Correct case
+
+
+const PathfinderContainer = () => {
+  const [activeComponent, setActiveComponent] = useState('BubbleSort');
+
+  
+
+  return (
+    <div className="page-container">
+      <div className="content-container">
+        
+      </div>
+    </div>
+  );
+};
+
+export default PathfinderContainer;
