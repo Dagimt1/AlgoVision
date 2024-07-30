5ce778f3c5949b998289ce2d26a8630b143a8bd9
M4dave <davidjohncruz@gmail.com>
2024-07-30 12:06:36 -0400

    fixed video, added brief description, added details.

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -1,29 +1,32 @@
-import { useState, useEffect, useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
-import { useNavigate } from 'react-router-dom';
-import './NavBar.css';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import "./NavBar.css";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const NavBar = () => {
   const { isLoggedIn, setIsLoggedIn, setAuthToken } = useContext(UserContext);
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
   const open = Boolean(anchorEl);
+  const navigateHome = () => {
+    navigate("/");
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
   const handleClickingAccount = () => {
-    navigate('/account');
+    navigate("/account");
   };
 
   const handleClickingLogOut = () => {
     setIsLoggedIn(false);
-    setAuthToken('');
-    navigate('/');
+    setAuthToken("");
+    navigate("/");
   };
 
   const handleClose = () => {
@@ -31,34 +34,36 @@ const NavBar = () => {
   };
 
   return (
-    <header className='header'>
-      <div className='container'>
-        <h1 className='title'>Algorithm Visualizer</h1>
+    <header className="header">
+      <div className="container">
+        <h1 onClick={navigateHome} className="title">
+          Algorithm Visualizer
+        </h1>
       </div>
       {isLoggedIn ? (
-        <div className='userAvatar'>
+        <div className="userAvatar">
           <Avatar onClick={handleClick} />
 
           <Menu
-            id='basic-menu'
+            id="basic-menu"
             anchorEl={anchorEl}
             open={open}
             onClose={handleClose}
             MenuListProps={{
-              'aria-labelledby': 'basic-button',
+              "aria-labelledby": "basic-button",
             }}
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
+              vertical: "bottom",
+              horizontal: "left",
             }}
             transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
+              vertical: "top",
+              horizontal: "left",
             }}
             slotProps={{
               paper: {
                 style: {
-                  transform: 'translate(-1.5vw, 10px)',
+                  transform: "translate(-1.5vw, 10px)",
                 },
               },
             }}
@@ -69,7 +74,7 @@ const NavBar = () => {
           </Menu>
         </div>
       ) : (
-        <button className='login-btn' onClick={() => navigate('/login')}>
+        <button className="login-btn" onClick={() => navigate("/login")}>
           Log in
         </button>
       )}
--- src/component/quickSort/QuickSort.jsx
+++ src/component/quickSort/QuickSort.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./quickSort.css"; // Import CSS file
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const QuickSortButton = ({ onClick, isActive }) => (
-  <button onClick={onClick} className={`quick-btn ${isActive ? 'active' : ''}`}>
+  <button onClick={onClick} className={`quick-btn ${isActive ? "active" : ""}`}>
     Quick Sort
   </button>
 );
 
 const QuickSort = () => {
   const QuickVideo = process.env.PUBLIC_URL + "./mp4/quicksort.mp4";
+  const navigate = useNavigate();
+  const navigateToLearnMore = () => {
+    navigate("/learnmore");
+  };
 
   return (
     <section className="section-container">
@@ -17,10 +21,14 @@ const QuickSort = () => {
         <video className="quick-vid" src={QuickVideo} autoPlay loop muted />
       </div>
       <p className="quick-description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum
-        dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit.
-        <Link to="/sorting" className="bubble-link"> Learn more</Link>
+        Quick Sort is a highly efficient sorting algorithm that uses a
+        divide-and-conquer strategy. It works by partitioning the array into
+        smaller segments, sorting those segments, and then combining them to
+        produce a sorted array.
+        <br />
+        <button className="learn-more-btn" onClick={navigateToLearnMore}>
+          Learn more
+        </button>
       </p>
     </section>
   );
