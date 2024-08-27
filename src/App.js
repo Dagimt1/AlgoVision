b5fb698462e0c03a32b19bd095df019f8f8660c2
M4dave <davidjohncruz@gmail.com>
2024-08-26 20:49:52 -0400

    updated features for sorting visualizer

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './component/landingpage/LandingPage.jsx';
-import Login from './component/Login/Login.jsx';
-import SignUp from './component/Login/SignUp.jsx';
-import Account from './component/Login/Account.jsx';
-import ResetPassword from './component/Login/ResetPassword.jsx';
-import SortContainer from './component/sortContainer/SortContainer.jsx';
-import MiddleTitle from './component/middletitle/MiddleTitle.jsx';
-import Sorting from './component/Sorting/Sorting.jsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./component/landingpage/LandingPage.jsx";
+import Login from "./component/Login/Login.jsx";
+import SignUp from "./component/Login/SignUp.jsx";
+import Account from "./component/Login/Account.jsx";
+import ResetPassword from "./component/Login/ResetPassword.jsx";
+import SortContainer from "./component/sortContainer/SortContainer.jsx";
+import MiddleTitle from "./component/middletitle/MiddleTitle.jsx";
+import Sorting from "./component/Sorting/Sorting.jsx";
 import AboutUs from './component/footer/AboutUs.jsx';
 import Testimonials from './component/footer/Testimonials.jsx';
-import Pathfinding from './component/Pathfinding/Pathfinding.js';
-import LearnMore from './component/learmore/LearnMore.jsx'
+import Pathfinding from "./component/Pathfinding/Pathfinding.js";
+import LearnMore from "./component/learmore/LearnMore.jsx";
 import ContactUs from './component/footer/ContactUs.jsx';
+import Footer from "./component/footer/Footer.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/resetpassword' element={<ResetPassword />} />
-        <Route path='/sortcontainer' element={<SortContainer />} />
-        <Route path='/middletitle' element={<MiddleTitle />} />
-        <Route path='/sorting' element={<Sorting />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/sortcontainer" element={<SortContainer />} />
+        <Route path="/middletitle" element={<MiddleTitle />} />
+        <Route path="/sorting" element={<Sorting />} />
         <Route path='/aboutus' element={<AboutUs />} />
-        <Route path='/pathfinding' element={<Pathfinding />} />
+        <Route path="/pathfinding" element={<Pathfinding />} />
         <Route path='/testimonials' element={<Testimonials />} />
-        <Route path='/learnmore' element={<LearnMore />} />
+        <Route path="/learnmore" element={<LearnMore />} />
         <Route path='/contactus' element={<ContactUs />} />
-
       </Routes>
+      {/* <Footer /> */}
     </Router>
   );
 }
